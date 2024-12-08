DrDecki/Aleph-One-PsVita

commit fe2109b9d2550709ba772c0acc6555473d733d7d

Remove ugly dummy variables and avoid extra copies at the same time in render tree

Source_Files/RenderMain/RenderSortPoly.cpp
@@ -173,17 +173,13 @@ void RenderSortPolyClass::sort_render_tree()
 		}
 		else /* this is a leaf, and we can remove it from the tree */
 		{
-			sorted_node_data *sorted_node;
-			
 //			dprintf("removed polygon #%d (#%d aliases)", leaf->polygon_index, alias_count);
 			
 			size_t Length = SortedNodes.size();
 			POINTER_DATA OldSNPointer = POINTER_CAST(SortedNodes.data());
 				
 			// Add a dummy object and check if the pointer got changed
-			sorted_node_data Dummy;
-			Dummy.polygon_index = NONE;			// Fake initialization to shut up CW
-			SortedNodes.push_back(Dummy);
+			sorted_node_data* sorted_node = &SortedNodes.emplace_back();
 			POINTER_DATA NewSNPointer = POINTER_CAST(SortedNodes.data());
 				
 			if (NewSNPointer != OldSNPointer)
@@ -194,7 +190,6 @@ void RenderSortPolyClass::sort_render_tree()
 					polygon_index_to_sorted_node[sorted_node->polygon_index]= sorted_node;
 				}
 			}
-			sorted_node = &SortedNodes[Length];
 			
 			sorted_node->polygon_index= leaf->polygon_index;
 			sorted_node->interior_objects= NULL;
@@ -406,9 +401,7 @@ clipping_window_data *RenderSortPolyClass::build_clipping_windows(
 					POINTER_DATA OldCWPointer = POINTER_CAST(ClippingWindows.data());
 					
 					// Add a dummy object and check if the pointer got changed
-					clipping_window_data Dummy;
-					Dummy.next_window = NULL;			// Fake initialization to shut up CW
-					ClippingWindows.push_back(Dummy);
+					clipping_window_data* window = &ClippingWindows.emplace_back();
 					POINTER_DATA NewCWPointer = POINTER_CAST(ClippingWindows.data());
 				
 					if (NewCWPointer != OldCWPointer)
@@ -427,8 +420,7 @@ clipping_window_data *RenderSortPolyClass::build_clipping_windows(
 								SortedNode.clipping_windows = (clipping_window_data *)(NewCWPointer + (POINTER_CAST(SortedNode.clipping_windows) - OldCWPointer));
 						}
 					}
-					clipping_window_data *window= &ClippingWindows[Length];
-					
+
 					/* handle maintaining the linked list of clipping windows */
 					if (Length > initial_cw_count)
 						ClippingWindows[Length-1].next_window = window;

Source_Files/RenderMain/RenderPlaceObjs.cpp
@@ -278,9 +278,7 @@ render_object_data *RenderPlaceObjsClass::build_render_object(
 				POINTER_DATA OldROPointer = POINTER_CAST(RenderObjects.data());
 				
 				// Add a dummy object and check if the pointer got changed
-				render_object_data Dummy;
-				Dummy.node = NULL;				// Fake initialization to shut up CW
-				RenderObjects.push_back(Dummy);
+				render_object = &RenderObjects.emplace_back();
 				POINTER_DATA NewROPointer = POINTER_CAST(RenderObjects.data());
 				
 				if (NewROPointer != OldROPointer)
@@ -301,7 +299,6 @@ render_object_data *RenderPlaceObjsClass::build_render_object(
 							SortedNode.exterior_objects = (render_object_data *)(NewROPointer + (POINTER_CAST(SortedNode.exterior_objects) - OldROPointer));
 					}
 				}
-				render_object= &RenderObjects[Length];
 				
 				render_object->clipping_windows = nullptr;
 				render_object->rectangle.flags= 0;

Source_Files/RenderMain/RenderVisTree.cpp
@@ -234,10 +234,7 @@ void RenderVisTreeClass::cast_render_ray(
 				// Cast the pointers to whatever size of integer the system uses.
 				size_t Length = Nodes.size();
 				
-				node_data Dummy;
-				Dummy.flags = 0;				// Fake initialization to shut up CW
-				Nodes.push_back(Dummy);
-				node = &Nodes[Length];		// The length here is the "old" length
+				node = &Nodes.emplace_back();
 				
 				*node_reference= node;
 				INITIALIZE_NODE(node, polygon_index, 0, parent, node_reference);
@@ -597,17 +594,14 @@ void RenderVisTreeClass::initialize_render_tree()
 {
 	// LP change: using growable list
 	Nodes.clear();
-	node_data Dummy;
-	Dummy.flags = 0;				// Fake initialization to shut up CW
-	Nodes.push_back(Dummy);
-	INITIALIZE_NODE(&Nodes[0], view->origin_polygon_index, 0, NULL, NULL);
+	INITIALIZE_NODE(&Nodes.emplace_back(), view->origin_polygon_index, 0, NULL, NULL);
 }
 
 /* ---------- initializing and calculating clip data */
 
 void RenderVisTreeClass::initialize_clip_data()
 {
-	ResetEndpointClips();
+	EndpointClips.resize(NUMBER_OF_INITIAL_ENDPOINT_CLIPS);
 	
 	/* set two default endpoint clips (left and right sides of screen) */
 	{
@@ -625,7 +619,7 @@ void RenderVisTreeClass::initialize_clip_data()
 		endpoint->x= view->screen_width;
 	}
 	
-	ResetLineClips();
+	LineClips.resize(NUMBER_OF_INITIAL_LINE_CLIPS);
 
 	/* set default line clip (top and bottom of screen) */
 	{
@@ -650,9 +644,7 @@ void RenderVisTreeClass::calculate_line_clipping_information(
 	uint16 clip_flags)
 {
 	// LP addition: extend the line-clip list
-	line_clip_data Dummy;
-	Dummy.flags = 0;			// Fake initialization to shut up CW
-	LineClips.push_back(Dummy);
+	line_clip_data* data = &LineClips.emplace_back();
 	size_t Length = LineClips.size();
 	assert(Length <= 32767);
 	assert(Length >= 1);
@@ -662,8 +654,6 @@ void RenderVisTreeClass::calculate_line_clipping_information(
 	// LP change: relabeling p0 and p1 so as not to conflict with later use
 	world_point2d p0_orig= get_endpoint_data(line->endpoint_indexes[0])->vertex;
 	world_point2d p1_orig= get_endpoint_data(line->endpoint_indexes[1])->vertex;
-	// LP addition: place for new line data
-	line_clip_data *data= &LineClips[LastIndex];
 
 	/* it’s possible (in fact, likely) that this line’s endpoints have not been transformed yet,
 		so we have to do it ourselves */
@@ -774,16 +764,13 @@ short RenderVisTreeClass::calculate_endpoint_clipping_information(
 		return NONE;
 	
 	// LP addition: extend the endpoint-clip list
-	endpoint_clip_data Dummy;
-	Dummy.flags = 0;			// Fake initialization to shut up CW
-	EndpointClips.push_back(Dummy);
+	endpoint_clip_data* data = &EndpointClips.emplace_back();
 	size_t Length = EndpointClips.size();
 	assert(Length <= 32767);
 	assert(Length >= 1);
 	size_t LastIndex = Length-1;
 
 	endpoint_data *endpoint= get_endpoint_data(endpoint_index);
-	endpoint_clip_data *data= &EndpointClips[LastIndex];
 	int32 x;
 
 	assert((clip_flags&(_clip_left|_clip_right))); /* must have a clip flag */
@@ -815,23 +802,4 @@ short RenderVisTreeClass::calculate_endpoint_clipping_information(
 	data->x= (short)PIN(x, 0, view->screen_width);
 	
 	return (short)LastIndex;
-}
-
-// LP addition: resetters for some of the lists:
-void RenderVisTreeClass::ResetEndpointClips(void)
-{
-	EndpointClips.clear();
-	endpoint_clip_data Dummy;
-	Dummy.flags = 0;			// Fake initialization to shut up CW
-	for (int k=0; k<NUMBER_OF_INITIAL_ENDPOINT_CLIPS; k++)
-		EndpointClips.push_back(Dummy);
-}
-
-void RenderVisTreeClass::ResetLineClips(void)
-{
-	LineClips.clear();
-	line_clip_data Dummy;
-	Dummy.flags = 0;			// Fake initialization to shut up CW
-	for (int k=0; k<NUMBER_OF_INITIAL_LINE_CLIPS; k++)
-		LineClips.push_back(Dummy);
 }

commit a8fa02df7015f0ab4d8d539b5e58ff10b6e294f6

Fix crash bug introduced in fe2109b9

Source_Files/RenderMain/RenderSortPoly.cpp
@@ -186,8 +186,8 @@ void RenderSortPolyClass::sort_render_tree()
 			{
 				// Update what uses the sorted-node pointers
 				for (size_t k=0; k<Length; k++) {
-					sorted_node = &SortedNodes[k];
-					polygon_index_to_sorted_node[sorted_node->polygon_index]= sorted_node;
+					auto node = &SortedNodes[k];
+					polygon_index_to_sorted_node[node->polygon_index] = node;
 				}
 			}
 			