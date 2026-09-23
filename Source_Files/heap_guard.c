0c2adb4138aa416181e0bd26d48be6404e5de252
DrDecki <289792529+DrDecki@users.noreply.github.com>
2026-09-23 21:46:09 +0200

    Guard the heap with a lock.

--- Source_Files/heap_guard.c
+++ Source_Files/heap_guard.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <psp2/kernel/sysmem.h>
 #include <psp2/kernel/clib.h>
+#include <psp2/kernel/threadmgr.h>
 
 void *__real_malloc(size_t size);
 void  __real_free(void *ptr);
@@ -12,6 +13,11 @@ void *__real_realloc(void *ptr, size_t size);
 static SceClibMspace hg_mspace = 0;
 static unsigned int  hg_lo = 0;
 static unsigned int  hg_hi = 0;
+static SceKernelLwMutexWork hg_lock;
+static int hg_lock_ok = 0;
+
+#define HG_LOCK()   do { if (hg_lock_ok) sceKernelLockLwMutex(&hg_lock, 1, NULL); } while (0)
+#define HG_UNLOCK() do { if (hg_lock_ok) sceKernelUnlockLwMutex(&hg_lock, 1); } while (0)
 
 static void hg_setup(void)
 {
@@ -31,6 +37,9 @@ static void hg_setup(void)
 
     hg_lo = (unsigned int)base;
     hg_hi = hg_lo + HG_POOL_SIZE;
+
+    if (sceKernelCreateLwMutex(&hg_lock, "hg_lock", 2, 0, NULL) == 0)
+        hg_lock_ok = 1;
 }
 
 __attribute__((constructor(101)))
@@ -47,34 +56,48 @@ static int hg_owns(const void *p)
 
 void *__wrap_malloc(size_t size)
 {
+    void *p;
     if (!hg_mspace) hg_setup();
     if (!hg_mspace) return __real_malloc(size);
-    return sceClibMspaceMalloc(hg_mspace, size);
+    HG_LOCK();
+    p = sceClibMspaceMalloc(hg_mspace, size);
+    HG_UNLOCK();
+    return p;
 }
 
 void *__wrap_calloc(size_t n, size_t size)
 {
+    void *p;
     if (!hg_mspace) hg_setup();
     if (!hg_mspace) {
-        void *p = __real_malloc(n * size);
+        p = __real_malloc(n * size);
         if (p) memset(p, 0, n * size);
         return p;
     }
-    return sceClibMspaceCalloc(hg_mspace, n, size);
+    HG_LOCK();
+    p = sceClibMspaceCalloc(hg_mspace, n, size);
+    HG_UNLOCK();
+    return p;
 }
 
 void *__wrap_memalign(size_t align, size_t size)
 {
+    void *p;
     if (!hg_mspace) hg_setup();
     if (!hg_mspace) return __real_malloc(size);
-    return sceClibMspaceMemalign(hg_mspace, align, size);
+    HG_LOCK();
+    p = sceClibMspaceMemalign(hg_mspace, align, size);
+    HG_UNLOCK();
+    return p;
 }
 
 void __wrap_free(void *ptr)
 {
     if (!ptr) return;
     if (hg_owns(ptr)) {
+        HG_LOCK();
         sceClibMspaceFree(hg_mspace, ptr);
+        HG_UNLOCK();
         return;
     }
     __real_free(ptr);
@@ -82,7 +105,13 @@ void __wrap_free(void *ptr)
 
 void *__wrap_realloc(void *ptr, size_t size)
 {
+    void *p;
     if (!ptr) return __wrap_malloc(size);
-    if (hg_owns(ptr)) return sceClibMspaceRealloc(hg_mspace, ptr, size);
+    if (hg_owns(ptr)) {
+        HG_LOCK();
+        p = sceClibMspaceRealloc(hg_mspace, ptr, size);
+        HG_UNLOCK();
+        return p;
+    }
     return __real_realloc(ptr, size);
 }
